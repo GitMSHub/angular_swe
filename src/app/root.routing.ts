91571cd0116827b61a20ec609137e63a288de2f4
Michael Sklors <michael-sklors@gmx.de>
2017-05-22 22:03:59 +0200

    App folder changed

--- src/app/root.routing.ts
+++ src/app/root.routing.ts
@@ -18,20 +18,20 @@
 import {ModuleWithProviders} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 
-import CreateBuchComponent from '../buch/create-buch/create-buch.component'
-import CreateBuchGuard from '../buch/create-buch/create-buch.guard'
-import DetailsBuchComponent from '../buch/details-buch/details-buch.component'
-import BalkendiagrammComponent from '../buch/diagramme/balkendiagramm.component'
-import LiniendiagrammComponent from '../buch/diagramme/liniendiagramm.component'
-import TortendiagrammComponent from '../buch/diagramme/tortendiagramm.component'
-import SucheBuecherComponent from '../buch/suche-buecher/suche-buecher.component'
-import UpdateBuchComponent from '../buch/update-buch/update-buch.component'
+import CreateKundeComponent from '../kunde/create-kunde/create-kunde.component'
+import CreateKundeGuard from '../kunde/create-kunde/create-kunde.guard'
+import DetailsKundeComponent from '../kunde/details-kunde/details-kunde.component'
+import BalkendiagrammComponent from '../kunde/diagramme/balkendiagramm.component'
+import LiniendiagrammComponent from '../kunde/diagramme/liniendiagramm.component'
+import TortendiagrammComponent from '../kunde/diagramme/tortendiagramm.component'
+import SucheKundenComponent from '../kunde/suche-kunden/suche-kunden.component'
+import UpdateKundeComponent from '../kunde/update-kunde/update-kunde.component'
 
 import {AdminGuard} from '../auth/admin.guard'
 import HomeComponent from '../home/home.component'
 
 export const HOME_PATH = ''
-export const DETAILS_BUCH_PATH = 'detailsBuch'
+export const DETAILS_KUNDE_PATH = 'detailsKunde'
 
 // https://angular.io/docs/ts/latest/guide/router.html
 /**
@@ -39,18 +39,18 @@ export const DETAILS_BUCH_PATH = 'detailsBuch'
  */
 const routes: Routes = [
     {path: HOME_PATH, component: HomeComponent},
-    {path: 'sucheBuecher', component: SucheBuecherComponent},
+    {path: 'sucheKunden', component: SucheKundenComponent},
     // id als Pfad-Parameter
-    {path: `${DETAILS_BUCH_PATH}/:id`, component: DetailsBuchComponent}, {
-        path: 'updateBuch/:id',
-        component: UpdateBuchComponent,
+    {path: `${DETAILS_KUNDE_PATH}/:id`, component: DetailsKundeComponent}, {
+        path: 'updateKunde/:id',
+        component: UpdateKundeComponent,
         canActivate: [AdminGuard],
     },
     {
-      path: 'createBuch',
-      component: CreateBuchComponent,
+      path: 'createKunde',
+      component: CreateKundeComponent,
       canActivate: [AdminGuard],
-      canDeactivate: [CreateBuchGuard],
+      canDeactivate: [CreateKundeGuard],
     },
     {
       path: 'balkendiagramm',
